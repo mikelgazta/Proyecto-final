37225b80d95b92665287422c35ee284b28bd39af
Mikel <91115057+mikelgazta@users.noreply.github.com>
2022-04-28 13:37:10 +0200

    Add files via upload
    tablas creadas pks y foreign keys por terminar

--- practica.sql
+++ practica.sql
@@ -16,25 +16,6 @@ size 1 M
 autoextend on next 10 K
 maxsize 1G;
 
-/*create user ventas identified by ventas
-default tablespace ventas
-temporary tablespace temp
-profile default
-quota 1M on ventas;
-
-GRANT CREATE SESSION TO ventas;
-
-
-CREATE PROFILE ventas_sesions LIMIT
-		SESSIONS_PER_USER 2
-		CPU_PER_SESSION UNLIMITED		
-		IDLE_TIME 20
-		CONNECT_TIME UNLIMITED
-		FAILED_LOGIN_ATTEMPTS 3
-		PASSWORD_LOCK_TIME 2;
-    
-ALTER USER ventas PROFILE ventas_sesions;*/
-
 create user administrador identified by administrador
 default tablespace administracion
 temporary tablespace temp
@@ -69,16 +50,97 @@ create profile profe_sessions LIMIT
 
 alter user profesor profile profe_sessions;
 
+drop table Academia cascade constraints;
+drop table Profesores cascade constraints;
+drop table Estudiantes cascade constraints;
+drop table Administradores cascade constraints;
+drop table Usuario cascade constraints;
+
+
 create table Academia
     (nombre varchar2 (20),
     direccion varchar2 (50),
     telf number (9));
 
-create table Persona
-    (dni varchar2 (9),
-    nombre varchar2 (20),
+create table Estudiantes
+    (dniE varchar2 (9),
+    nombreE varchar2 (20),
     apellidos varchar2 (50),
     direccion varchar2(50),
-    telf number(9));
+    telf number(9),
+    idioma varchar2(10),
+    idUsuario varchar2(5));
 
+create table Profesores
+    (dniP varchar2 (9),
+    nombreP varchar2 (20),
+    apellidos varchar2 (50),
+    direccion varchar2(50),
+    telf number(9),
+    especialidad varchar2(30),
+    idUsuario varchar2(5));
+    
+create table Administradores
+    (dniA varchar2 (9),
+    nombreA varchar2 (20),
+    apellidos varchar2 (50),
+    direccion varchar2(50),
+    telf number(9),
+    idUsuario varchar2(5));
 
+create table Usuarios
+    (idUsuario varchar2(5),
+    nombreU varchar2(20),
+    contraseña varchar2 (15),
+    rol char (1));
+    
+create table Clase
+    (id_clase number(2),
+    dniP varchar2(20),
+    nombreE varchar2(20),
+    idioma varchar2(30));
+    
+create table Titulo
+    (nivel varchar2(10),
+    nombreE varchar2(20),
+    idioma varchar2(20),
+    cuota number (3,2));
+    
+alter table Usuarios
+add constraint pk_user
+primary key (idUsuario);
+
+alter table  Estudiantes
+add constraint pk_est
+primary key (dniE);
+
+alter table  Profesores
+add constraint pk_profe
+primary key (dniP);
+
+alter table  Administradores
+add constraint pk_admin
+primary key (dniA);
+
+alter table Titulo
+add constraint pk_titulo
+primary key (dniE);
+
+alter table clase
+add constraint pk_clase
+primary key (id_clase);
+
+alter table Estudiantes
+add constraint fk_est
+foreign key (idUsuario)
+references Usuarios (idUsuario);
+
+alter table Profesores
+add constraint fk_profe
+foreign key (idUsuario)
+references Usuarios (idUsuarios);
+
+alter table Administradores
+add constraint fk_amdin
+foreign key (idUsuario)
+references Usuarios (idUsuario);
